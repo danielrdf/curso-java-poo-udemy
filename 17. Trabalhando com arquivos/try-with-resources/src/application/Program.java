3f36d3f6ae7033f632745e608c225e9e376b20de
Daniel Fonseca <danielrdf@gmail.com>
2022-10-15 11:50:05 -0300

    Create try-with-resources

---
+++ 17. Trabalhando com arquivos/try-with-resources/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		String path = "/Users/danielfonseca/Documents/CURSOS E TUTORIAIS/Ûdemy/curso-java-poo-udemy/17. Trabalhando com arquivos/in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+			
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+	}
+
+}
