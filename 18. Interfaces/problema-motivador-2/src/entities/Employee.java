e2c1b6c6905bc4d90c8c946915b1184486986f11
Daniel Fonseca <danielrdf@gmail.com>
2022-10-28 13:24:44 -0300

    Update Employee.java

--- 18. Interfaces/problema-motivador-2/src/entities/Employee.java
+++ 18. Interfaces/problema-motivador-2/src/entities/Employee.java
@@ -33,7 +33,7 @@ public class Employee implements Comparable<Employee> {
 	
 	@Override
 	public int compareTo(Employee other) {
-		return -salary.compareTo(other.getSalary());
+		return -salary.compareTo(other.getSalary()); // ordem decrescente de salário
 	}
 	
 }
